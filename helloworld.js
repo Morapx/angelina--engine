3e118ff70e38a635220d54c44abe3b7f3e6c4221
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-10-29 12:47:37 -0700

    .545445458

--- helloworld.js
+++ helloworld.js
@@ -10,7 +10,9 @@ const main = ()=>{
 
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    
+
+    //el paradigma mas basica en los shadders es el lambert que es el tipo mas simpple de iluminacion //
+
 }
 
 window.onload = main;
