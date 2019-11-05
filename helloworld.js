ba4cc66d2ab7215cd11b98cdf0bd9b6c90e20cb9
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-11-05 12:32:51 -0700

    4565

--- helloworld.js
+++ helloworld.js
@@ -72,6 +72,15 @@ const initBuffers = gl =>{
     };
 }
 
+const drawScene = (gl, programInfo, buffers)=>{
+    gl.clearColor(0, 0, 0, 1);
+    gl.clearDepth(1);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
+
 const main = ()=>{
     const gl = canvas.getContext('webgl2');
 
@@ -96,9 +105,7 @@ const main = ()=>{
 
     const buffers = initBuffers(gl);
 
-
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    drawScene(gl, programInfo, buffers);
 }
 
 window.onload = main;
