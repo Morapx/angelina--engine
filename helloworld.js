536d3546f54b1cb9a461a74a42bb376fbb3025bb
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-11-05 12:44:08 -0700

    otro

--- helloworld.js
+++ helloworld.js
@@ -79,7 +79,20 @@ const drawScene = (gl, programInfo, buffers)=>{
     gl.depthFunc(gl.LEQUAL);
 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const fieldOfview = 45 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100;
+
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(
+        projectionMatrix, fieldOfview, aspect, zNear, zFar
+    );
 }
+
+
 
 const main = ()=>{
     const gl = canvas.getContext('webgl2');
